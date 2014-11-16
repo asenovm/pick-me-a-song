b349d2045b51c748eca4000e2fe58871aaf460e6
asenovm <asenov.m@gmail.com>
2014-11-16 17:51:59 +0200

    * Scraper seems to be fully functional now

--- data/scraper.js
+++ data/scraper.js
@@ -1,8 +1,54 @@
 var fs = require('fs'),
     config = JSON.parse(fs.readFileSync('config.json')),
-    LastFmNode = require('lastfm').LastFmNode,
-    lastfm = new LastFmNode({
+    LastfmAPI = require('lastfmapi'),
+    _ = require('underscore'),
+    db = require('./db'),
+    lastfm = new LastfmAPI({
         api_key: config.apiKey,
-        secret: config.secret,
-        useragent: config.userAgent
+        secret: config.secret
     });
+
+startCrawling();
+
+function startCrawling() {
+    crawl(config.crawlRoot);
+}
+
+function crawl(user) {
+    visitNode(user, config.startPage, config.startPage);
+    lastfm.user.getFriends({ user: user }, function (err, result) {
+        if(err) {
+            console.log('an error occurred');
+            console.dir(err);
+        } else {
+            var friends = result.user;
+            _.each(friends, function (friend) {
+                db.hasUser(friend.name, function (err, result) {
+                    if(!result && friend.name) {
+                        crawl(friend.name);
+                    }
+                });
+            });
+        }
+    });
+}
+
+function visitNode (user, page, totalPages) {
+    if (page > totalPages) {
+        return;
+    }
+
+    lastfm.user.getLovedTracks({ user: user, page: page, limit: config.itemsPerPage }, function (err, result) {
+        if (err) {
+            console.log('get tracks err is ', err);
+        } else if (result['@attr']) {
+            var meta = result['@attr'],
+                totalPages = meta.totalPages,
+                tracks = _.isArray(result.track) ? result.track : [result.track];
+
+            db.updateUserProfile({ name: user, tracks: tracks }, function (err, result) {
+                visitNode(user, page + 1, totalPages);
+            });
+        }
+    });
+}
