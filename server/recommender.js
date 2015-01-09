d9dfbc70fb9f0e3681c5cf9f7aa5392f55f07f77
asenovm <asenov.m@gmail.com>
2015-01-09 22:20:48 +0200

    * Computing the similarity between the saved users and the current one using cosine similarity

--- server/recommender.js
+++ server/recommender.js
@@ -2,14 +2,43 @@ var _ = require('underscore'),
     db = require('./db');
 
 exports.getRecommendationsFor = function (artists, callback) {
-    db.retrieveAllUsers(artists, function (err, similarUsers) {
+    db.retrieveAllUsers(artists, function (err, users) {
         if(err) {
             console.log('an error occurred');
             console.dir(err);
         } else {
-            console.dir(_.map(similarUsers, function (user) {
-                return user.user;
-            }));
+            _.each(users, function (user) {
+                var tracks = [];
+                _.each(artists, function (artist) {
+                    tracks = _.union(tracks, _.filter(user.tracks, function (track) {
+                        return track.artist.name === artist.name;
+                    }));
+                });
+
+                var nominator = _.reduce(tracks, function (memo, track) {
+                    var trackAuthor = _.find(artists, function (artist) {
+                        return artist.name === track.artist.name;
+                    });
+
+                    return memo + track.playcount * trackAuthor.score
+                }, 0);
+
+                var userDenominator = _.reduce(tracks, function (memo, track) {
+                    return memo + track.playcount * track.playcount;
+                }, 0);
+
+                var artistDenominator = _.reduce(tracks, function (memo, track) {
+                    var trackAuthor = _.find(artists, function (artist) {
+                        return artist.name === track.artist.name;
+                    });
+
+                    return memo + trackAuthor.score * trackAuthor.score;
+                }, 0);
+
+                var similarity = nominator / (Math.sqrt(userDenominator) * Math.sqrt(artistDenominator));
+
+                console.log('similarity metric is ', similarity);
+            });
         }
     });
 };
