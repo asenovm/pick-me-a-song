eca4db1cfb96d6465de92471e7af7bba737c5692
asenovm <asenov.m@gmail.com>
2015-01-11 19:02:18 +0200

    * Added a node server that serves the static files

---
+++ server-static.js
@@ -0,0 +1,8 @@
+var static = require('node-static'),
+    fileServer = new static.Server('./web');
+
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        fileServer.serve(request, response);
+    }).resume();
+}).listen(8080);
