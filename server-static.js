f938d2e06e061d427b877c55fc8d1fca886fba93
asenovm <asenov.m@gmail.com>
2015-01-11 19:19:06 +0200

    * The static-file-server is now listening to the port heroku is assigning to the app

--- server-static.js
+++ server-static.js
@@ -5,4 +5,4 @@ require('http').createServer(function (request, response) {
     request.addListener('end', function () {
         fileServer.serve(request, response);
     }).resume();
-}).listen(9090);
+}).listen(process.env.PORT || 8080);
