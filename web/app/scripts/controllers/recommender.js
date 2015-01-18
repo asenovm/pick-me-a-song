8ba305d733eba686120f131fcd9f5f59e8000d0f
asenovm <asenov.m@gmail.com>
2015-01-18 15:06:37 +0200

    * Tweaked the loading screen a little bit

--- web/app/scripts/controllers/recommender.js
+++ web/app/scripts/controllers/recommender.js
@@ -8,14 +8,12 @@ angular.module('pickMeASong')
     $scope.artists = [{ score: 1 }];
 
     $scope.showRecommendations = function () {
-        //$location.path(PATH_RECOMMENDATIONS);
+        $scope.isLoading = false;
+        $location.path(PATH_RECOMMENDATIONS);
     };
 
     $scope.getRecommendations = function () {
         $scope.isLoading = true;
-        setTimeout(function () {
-            $scope.isLoading = false;
-        }, 30000);
         recommendationsService.fetchRecommendations($scope.artists).finally($scope.showRecommendations);
     };
 
