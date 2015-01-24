4da9e86810ff7ce619483b08ee579f1281889114
asenovm <asenov.m@gmail.com>
2015-01-24 20:38:44 +0200

    * Refactored the recommender a bit more

---
+++ contentBasedRecommender.js
@@ -0,0 +1,74 @@
+var _ = require('underscore'),
+    db = require('./db');
+
+exports.getRecommendations = function (artists, callback) {
+    db.getTagsForArtists(artists, function (err, taggedArtists) {
+        if(err) {
+            callback(err, []);
+        } else {
+            var tags = [],
+                userDocument = {},
+                differentTags = {},
+                artistsNames = _.map(artists, function (artist) {
+                    return artist.name;
+                });
+
+            _.each(taggedArtists, function (artist) {
+                var artistTags = _.first(artist.tags, 3),
+                    scoredArtist = _.findWhere(artists, { name: artist.name }),
+                    artistScore = parseInt(scoredArtist.score, 10);
+
+                _.each(artistTags, function (tag) {
+                    userDocument[tag.name] = userDocument[tag.name] || 0;
+                    userDocument[tag.name] += artistScore;
+                    differentTags[tag.name] = 1;
+                });
+
+                tags = _.union(tags, artistTags);
+            });
+
+            db.getTracksForTags(tags, function (err, tracks) {
+                _.each(tracks, function (track) {
+                    var tags = _.first(track.tags, 6),
+                        trackDocument = {};
+
+                    _.each(tags, function (tag) {
+                        var count = parseInt(tag.count, 10);
+                        trackDocument[tag.name] = trackDocument[tag.name] || 0;
+                        trackDocument[tag.name] += count;
+                        differentTags[tag.name] = 1;
+                    });
+
+                    var nominator = 0,
+                        userDenominator = 0,
+                        trackDenominator = 0;
+
+                    _.each(userDocument, function (value, key) {
+                        if(trackDocument[key]) {
+                            nominator += value * trackDocument[key];       
+                        }
+                        userDenominator += value * value;
+                    });
+
+                    _.each(trackDocument, function (value, key) {
+                        trackDenominator += value * value;
+                    });
+
+                    userDenominator = Math.sqrt(userDenominator);
+                    trackDenominator = Math.sqrt(trackDenominator);
+
+                    track.similarity = nominator / (userDenominator * trackDenominator);
+                });
+
+                var recommendedTracks = _.first(_.sortBy(_.filter(tracks, function (track) {
+                    return !_.contains(artistsNames, track.artist.name);
+                }), function (track) {
+                    return -track.similarity;
+                }), 20);
+
+                callback(false, recommendedTracks);
+            });
+        }
+    });
+}
+
--- recommender.js
+++ recommender.js
@@ -4,84 +4,14 @@ var _ = require('underscore'),
     MIN_RECOMMENDED_ITEMS_PER_USER = 2,
     ALGORITHM_TYPE_COLLABORATIVE_FILTERING = 'collaborativeFiltering',
     collaborativeRecommender = require('./collaborativeRecommender'),
+    contentBasedRecommender = require('./contentBasedRecommender'),
     EPS = 0.001;
 
 exports.getRecommendationsFor = function (artists, algorithmType, neighboursCount, recommendedItemsCount, callback) {
     if(algorithmType === ALGORITHM_TYPE_COLLABORATIVE_FILTERING) {
         collaborativeRecommender.getRecommendations(artists, neighboursCount, recommendedItemsCount, callback);
     } else {
-        getContentFilteringRecommendations(artists, callback);
+        contentBasedRecommender.getRecommendations(artists, callback);
     }
 };
-
-function getContentFilteringRecommendations(artists, callback) {
-    db.getTagsForArtists(artists, function (err, taggedArtists) {
-        if(err) {
-            callback(err, []);
-        } else {
-            var tags = [],
-                userDocument = {},
-                differentTags = {},
-                artistsNames = _.map(artists, function (artist) {
-                    return artist.name;
-                });
-
-            _.each(taggedArtists, function (artist) {
-                var artistTags = _.first(artist.tags, 3),
-                    scoredArtist = _.findWhere(artists, { name: artist.name }),
-                    artistScore = parseInt(scoredArtist.score, 10);
-
-                _.each(artistTags, function (tag) {
-                    userDocument[tag.name] = userDocument[tag.name] || 0;
-                    userDocument[tag.name] += artistScore;
-                    differentTags[tag.name] = 1;
-                });
-
-                tags = _.union(tags, artistTags);
-            });
-
-            db.getTracksForTags(tags, function (err, tracks) {
-                _.each(tracks, function (track) {
-                    var tags = _.first(track.tags, 6),
-                        trackDocument = {};
-
-                    _.each(tags, function (tag) {
-                        var count = parseInt(tag.count, 10);
-                        trackDocument[tag.name] = trackDocument[tag.name] || 0;
-                        trackDocument[tag.name] += count;
-                        differentTags[tag.name] = 1;
-                    });
-
-                    var nominator = 0,
-                        userDenominator = 0,
-                        trackDenominator = 0;
-
-                    _.each(userDocument, function (value, key) {
-                        if(trackDocument[key]) {
-                            nominator += value * trackDocument[key];       
-                        }
-                        userDenominator += value * value;
-                    });
-
-                    _.each(trackDocument, function (value, key) {
-                        trackDenominator += value * value;
-                    });
-
-                    userDenominator = Math.sqrt(userDenominator);
-                    trackDenominator = Math.sqrt(trackDenominator);
-
-                    track.similarity = nominator / (userDenominator * trackDenominator);
-                });
-
-                var recommendedTracks = _.first(_.sortBy(_.filter(tracks, function (track) {
-                    return !_.contains(artistsNames, track.artist.name);
-                }), function (track) {
-                    return -track.similarity;
-                }), 20);
-
-                callback(false, recommendedTracks);
-            });
-        }
-    });
-}
 
