f71d194fd599265555f52c1c5963a2ba8d9bca49
asenovm <asenov.m@gmail.com>
2015-02-14 21:42:34 +0200

    * Removing duplicated from the recommendations

--- recommenderUtil.js
+++ recommenderUtil.js
@@ -2,12 +2,14 @@ var _ = require('underscore'),
     COUNT_RECOMMENDED_TRACKS = 20;
 
 exports.getRecommendationsFromPredictions = function (predictedRatings, previousRecommendations) {
-    return _.first(_.sortBy(_.filter(predictedRatings, function (track) {
+    return _.first(_.sortBy(_.uniq(_.filter(predictedRatings, function (track) {
         var contains = false;
         _.each(previousRecommendations, function (recommendation) {
             contains = contains || (recommendation.artist.name === track.artist.name && recommendation.name === track.name);
         });
         return !contains;
+    }), false, function (track) {
+        return track.artist.name + track.name;
     }), function (track) {
         return -track.score;
     }), COUNT_RECOMMENDED_TRACKS);
